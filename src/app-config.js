87c79bd20c8868e3975284fde89ba95405504fd9
Jan Chalupa <janchalupa@outlook.cz>
2019-11-22 16:49:12 +0100

    Initial commit: New version.

--- src/app-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'Discussion Watcher',
-    nameShort: 'Discussion Watcher',
-    description: 'Some description.',
-    accentColor: '#ff5252',
-    developerName: 'Jan Chalupa',
-    developerUrl: 'https://www.honzachalupa.cz/',
-    caching: true
-};
